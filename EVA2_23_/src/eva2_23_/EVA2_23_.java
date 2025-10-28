43e85d82dc1d10eb6263b4bed430c976ed80550b
CesarMarquezMedrano <L25550717@chihuahua2.tecnm.mx>
2025-10-27 22:01:58 -0600

    Add files via upload

---
+++ EVA2_23_/src/eva2_23_/EVA2_23_.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_23_;
+
+/**
+ *
+ * @author cmarq
+ */
+public class EVA2_23_ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        while(true){//ciclo infinito
+            System.out.println("CESAR MARQUEZ");
+        }
+        
+    }
+    
+}
